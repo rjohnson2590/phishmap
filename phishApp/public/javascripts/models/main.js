b6317b4b02577921c5e9276dc5b0fe8792ffd639
[Richard Johnson] <[rjohnson2590@gmail.com]>
2016-02-14 23:19:51 -0800

    Moved the google maps api call out of search function and made it it's own function.

--- phishApp/public/javascripts/models/main.js
+++ phishApp/public/javascripts/models/main.js
@@ -23,34 +23,35 @@ var phishYearModel = Backbone.Model.extend({
                 //});
                 console.log('data',phishDataParsed);
                 self.set('data',phishDataParsed.data[0].location );
-                $.ajax({
-                    url: "https://maps.googleapis.com/maps/api/geocode/json?address="+ phishDataParsed.data[0].location +"&key=AIzaSyAWVdwwYaifvHtZSbG6AKI1IF5HwLPzeHw",
-                    beforeSend: function( xhr ) {
-                        xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
-                    }
-                })
-                    .done(function(phishCor){
-                        var phishCorParsed = JSON.parse(phishCor);
-                        console.log('phishCor',phishCorParsed);
-                        var lat = phishCorParsed.results[0].geometry.location.lat;
-                        var lng = phishCorParsed.results[0].geometry.location.lng;
-                        var map = new google.maps.Map(document.getElementById('map'), {
-                            zoom: 5,
-                            center: {lat: lat, lng: lng}
-                        });
-
-                        var marker = new google.maps.Marker({
-                            position: {lat: lat, lng: lng},
-                            map: map,
-                            title: 'Hello World!'
-                        });
-
-                    })
-
+                self.map(phishDataParsed.data[0].location);
             });
+    },
 
+    map : function(coordinates){
+        $.ajax({
+                url: "https://maps.googleapis.com/maps/api/geocode/json?address="+ coordinates +"&key=AIzaSyAWVdwwYaifvHtZSbG6AKI1IF5HwLPzeHw",
+                beforeSend: function( xhr ) {
+                    xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
+                }
+            })
+            .done(function(phishCor){
+                var phishCorParsed = JSON.parse(phishCor);
+                console.log('phishCor',phishCorParsed);
+                var lat = phishCorParsed.results[0].geometry.location.lat;
+                var lng = phishCorParsed.results[0].geometry.location.lng;
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 4,
+                    center: {lat: lat, lng: lng}
+                });
+
+                var marker = new google.maps.Marker({
+                    position: {lat: lat, lng: lng},
+                    map: map,
+                    title: 'Hello World!'
+                });
+            });
     }
 
-})
+});
 
 var phishYear = new phishYearModel({});
