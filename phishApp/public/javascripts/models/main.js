d0ac261e30bf0ccd385da536001f4d38f44ad986
[Richard Johnson] <[rjohnson2590@gmail.com]>
2016-02-14 23:07:05 -0800

    First veneue location for the year entered appears.

--- phishApp/public/javascripts/models/main.js
+++ phishApp/public/javascripts/models/main.js
@@ -17,8 +17,35 @@ var phishYearModel = Backbone.Model.extend({
                 }
             })
             .done(function( phishData ) {
-                    var phishDataParsed = JSON.parse(phishData);
-                    self.set('data',phishDataParsed.data[0].location );
+                var phishDataParsed = JSON.parse(phishData);
+                //phishDataParsed.data.forEach(function(e,i){
+                //    self.set('data', e.location);
+                //});
+                console.log('data',phishDataParsed);
+                self.set('data',phishDataParsed.data[0].location );
+                $.ajax({
+                    url: "https://maps.googleapis.com/maps/api/geocode/json?address="+ phishDataParsed.data[0].location +"&key=AIzaSyAWVdwwYaifvHtZSbG6AKI1IF5HwLPzeHw",
+                    beforeSend: function( xhr ) {
+                        xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
+                    }
+                })
+                    .done(function(phishCor){
+                        var phishCorParsed = JSON.parse(phishCor);
+                        console.log('phishCor',phishCorParsed);
+                        var lat = phishCorParsed.results[0].geometry.location.lat;
+                        var lng = phishCorParsed.results[0].geometry.location.lng;
+                        var map = new google.maps.Map(document.getElementById('map'), {
+                            zoom: 5,
+                            center: {lat: lat, lng: lng}
+                        });
+
+                        var marker = new google.maps.Marker({
+                            position: {lat: lat, lng: lng},
+                            map: map,
+                            title: 'Hello World!'
+                        });
+
+                    })
 
             });
 
